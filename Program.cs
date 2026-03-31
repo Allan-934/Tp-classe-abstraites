f9f1325991261adc988a33bcaae3c98127069463
Decugis Allan <allan.decugis@edouardgand.fr>
2026-03-31 11:27:44 +0200

    Exercice 4 -b

--- Program.cs
+++ Program.cs
@@ -12,6 +12,10 @@ class Program
         o1.AfficherInfos();
         Console.WriteLine(o1);
 
+        // Exercice 4 Creation d'un new ordi avec une date diff 
+        Ordinateur o2 = new Ordinateur("HP", "Pavilion", new DateTime(2018, 3, 15), 14);
+        o2.CalculerAnciennete();
+
         Console.WriteLine("\n-----------------------------\n");
 
         //Exercice 3 : Création d'un nouveau smartphone + test des méthodes
@@ -20,5 +24,9 @@ class Program
         s1.AfficherType();
         s1.AfficherGarantie();
         s1.AfficherInfos();
+
+        // Exercice 4 Creation d'un new smartphone avec une date diff 
+        Smartphone s2 = new Smartphone("Samsung", "Galaxy S20", new DateTime(2020, 2, 11), 4);
+        s2.CalculerAnciennete();
     }
 }
